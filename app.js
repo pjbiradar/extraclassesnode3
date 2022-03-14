3fb7795610f3b68280301f751e59f972c4e9caa5
pjbiradar <poojabiradar2412@gmail.com>
2022-03-14 11:28:27 +0530

    anytext

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const app=express()
 
 app.use("/api/v1",router)
 app.use(cors())
+app.get('/user',(req,res)=>{
+    res.send("hello")
+})
+app.put('/user',(req,res)=>{
+    res.send("hello")
+})
+app.delete('/user',(req,res)=>{
+    res.send("hello")
+})
+app.post('/user',(req,res)=>{
+    res.send("hello")
+})
 
 app.listen(4000,()=>{
     console.log("success hehad");
